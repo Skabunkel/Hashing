c20f4f9519443a11523efafef98ef5a99d50d2bf
Skabunkel <>
2019-02-27 18:40:13 +0100

    auto formatting broke something wtf

--- blake2b.c
+++ blake2b.c
@@ -1,36 +1,33 @@
 /*
- *   My personal implementation of the blake2B hash algorithm in C99.
- *   based on this psudocode
- * https://en.wikipedia.org/wiki/BLAKE_%28hash_function%29#BLAKE2 and refrences
- * from https://github.com/BLAKE2/BLAKE2/
- *
- *   This implementation is not a full implimentation since it can only access
- * 2^64 bytes instead of the full 2^128 bytes the psudocode refrences For a safe
- * and tested implementation see https://github.com/BLAKE2/BLAKE2/
- *
- *   This implementation is for educational purposes and should be considered
- * unsafe for password hashing. I would like to make it safe, however i need to
- * read more about what that means and how it is achived.
- *
- *   I have not tested it on a big endian machine so i hope it will work. don't
- * know though. Now that i think about it probably not.
- */
+*   My personal implementation of the blake2B hash algorithm in C99.
+*   based on this psudocode https://en.wikipedia.org/wiki/BLAKE_%28hash_function%29#BLAKE2
+*   and refrences from https://github.com/BLAKE2/BLAKE2/
+*
+*   This implementation is not a full implimentation since it can only access 2^64 bytes instead of the full 2^128 bytes the psudocode refrences
+*   For a safe and tested implementation see https://github.com/BLAKE2/BLAKE2/
+*
+*   This implementation is for educational purposes and should be considered unsafe for password hashing.
+*   I would like to make it safe, however i need to read more about what that means and how it is achived.
+*
+*   I have not tested it on a big endian machine so i hope it will work. don't know though. Now that i think about it probably not.
+*/
+
 
 #include "blake2b.h"
 #define BLAKE2B_CONSTANT_BLOCKBYTES 128
-#define BLAKE2B_CONSTANT_HASH_VECTOR_SIZE 8
+#define BLAKE2B_CONSTANT_HASH_VECTOR_SIZE  8
 
-bool Is_Big_Endian() 
+bool Is_Big_Endian()
 {
   union {
     uint32_t i;
     char c[4];
-  } bint = {0x01020304};
+  } bint = { 0x01020304 };
 
   return bint.c[0] == 1;
 }
 
-void Flip_Uint64_Bytes(uint64_t *value) 
+void Flip_Uint64_Bytes(uint64_t *value)
 {
   uint64_t standin = *value << 56;
   standin |= (*value & 0xff00) << 40;
@@ -43,30 +40,29 @@ void Flip_Uint64_Bytes(uint64_t *value)
   *value = standin;
 }
 
-static const uint8_t sigma[10][16] = 
+static const uint8_t sigma[10][16] =
 {
-    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-    {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
-    {11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
-    {7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
-    {9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13},
-    {2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
-    {12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
-    {13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
-    {6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
-    {10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0}
+  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
+{ 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 },
+{ 11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4 },
+{ 7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8 },
+{ 9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13 },
+{ 2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9 },
+{ 12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11 },
+{ 13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10 },
+{ 6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5 },
+{ 10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0 }
 };
 
-static const uint64_t Blake2BIV[BLAKE2B_CONSTANT_HASH_VECTOR_SIZE] = 
+static const uint64_t Blake2BIV[BLAKE2B_CONSTANT_HASH_VECTOR_SIZE] =
 {
-    0x6A09E667F3BCC908ULL, 0xBB67AE8584CAA73BULL, 
-    0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL, 
-    0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
-    0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
+  0x6A09E667F3BCC908ULL, 0xBB67AE8584CAA73BULL,
+  0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
+  0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
+  0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
 };
 
-typedef struct Blake2BState 
-{
+typedef struct Blake2BState {
   uint64_t stateVector[BLAKE2B_CONSTANT_HASH_VECTOR_SIZE];
   uint8_t targetLength;
   uint8_t keyLength;
@@ -75,12 +71,12 @@ typedef struct Blake2BState
   uint8_t blocks[BLAKE2B_CONSTANT_BLOCKBYTES];
 } Blake2BState;
 
-uint64_t Shift64(const uint64_t value, int bits) 
+uint64_t Shift64(const uint64_t value, int bits)
 {
   return ((value >> bits) | (value << (64 - bits)));
 }
 
-void Mix(uint64_t *va, uint64_t *vb, uint64_t *vc, uint64_t *vd, uint64_t x, uint64_t y) 
+void Mix(uint64_t *va, uint64_t *vb, uint64_t *vc, uint64_t *vd, uint64_t x, uint64_t y)
 {
   *va = *va + *vb + x;
   *vd = Shift64(*vd ^ *va, 32);
@@ -95,11 +91,11 @@ void Mix(uint64_t *va, uint64_t *vb, uint64_t *vc, uint64_t *vd, uint64_t x, uin
   *vb = Shift64(*vb ^ *vc, 63);
 }
 
-void Blake2B_Compress(Blake2BState *state, const bool isLastBlock) 
+void Blake2B_Compress(Blake2BState *state, const bool isLastBlock)
 {
   uint64_t v[16];
 
-  for (int i = 0; i < 8; i++) 
+  for (int i = 0; i < 8; i++)
   {
     v[i] = state->stateVector[i];
     v[i + 8] = Blake2BIV[i];
@@ -107,15 +103,15 @@ void Blake2B_Compress(Blake2BState *state, const bool isLastBlock)
 
   v[12] ^= state->readblock;
 
-  if (isLastBlock) 
+  if (isLastBlock)
   {
     v[14] ^= 0xFFFFFFFFFFFFFFFF;
   }
 
-  uint64_t *messageVector = (uint64_t *)state->blocks;
+  uint64_t *messageVector = (uint64_t*)state->blocks;
 
   uint8_t st = 0;
-  for (int i = 0; i < 12; i++) 
+  for (int i = 0; i < 12; i++)
   {
     st = (uint8_t)(i % 10);
 
@@ -130,17 +126,13 @@ void Blake2B_Compress(Blake2BState *state, const bool isLastBlock)
     Mix(&v[3], &v[4], &v[9], &v[14], messageVector[sigma[st][14]], messageVector[sigma[st][15]]);
   }
 
-  for (int i = 0; i < 8; i++) 
+  for (int i = 0; i < 8; i++)
   {
     state->stateVector[i] ^= v[i] ^ v[i + 8];
   }
 }
 
-bool Blake2B_Init(Blake2BState *state, const uint8_t outLength,
-                  const uint8_t *key, const uint8_t keyLength,
-                  const uint8_t *salt, const uint8_t saltLength,
-                  const uint8_t *personalization,
-                  const uint8_t personalizationLength) 
+bool Blake2B_Init(Blake2BState *state, const uint8_t outLength, const uint8_t *key, const uint8_t keyLength, const uint8_t *salt, const uint8_t saltLength, const uint8_t *personalization, const uint8_t personalizationLength)
 {
   state->stateVector[1] = Blake2BIV[1];
   state->stateVector[2] = Blake2BIV[2];
@@ -152,21 +144,21 @@ bool Blake2B_Init(Blake2BState *state, const uint8_t outLength,
 
   // Salt is 16 bytes at ofset 32.
   // Personalization is 16 bytes at ofset 48.
-  uint8_t hashSt[BLAKE2B_CONSTANT_OUTANDKEYLENGTH] = {outLength, keyLength, 0x01, 0x01};
+  uint8_t hashSt[BLAKE2B_CONSTANT_OUTANDKEYLENGTH] = { outLength, keyLength, 0x01, 0x01 };
   memset(hashSt + 4, 0, BLAKE2B_CONSTANT_OUTANDKEYLENGTH - 4);
-  if (saltLength > 0) 
+  if (saltLength > 0)
   {
     memcpy(hashSt + 32, salt, saltLength);
   }
 
-  if (personalizationLength > 0) 
+  if (personalizationLength > 0)
   {
     memcpy(hashSt + 48, personalization, personalizationLength);
   }
 
-  uint8_t *IV0 = (uint8_t *)Blake2BIV;
-  uint8_t *result = (uint8_t *)state->stateVector;
-  for (int i = 0; i < BLAKE2B_CONSTANT_OUTANDKEYLENGTH; i++) 
+  uint8_t *IV0 = (uint8_t*)Blake2BIV;
+  uint8_t *result = (uint8_t*)state->stateVector;
+  for (int i = 0; i < BLAKE2B_CONSTANT_OUTANDKEYLENGTH; i++)
   {
     result[i] = IV0[i] ^ hashSt[i];
   }
@@ -176,7 +168,7 @@ bool Blake2B_Init(Blake2BState *state, const uint8_t outLength,
 
   memset(state->blocks, 0, BLAKE2B_CONSTANT_BLOCKBYTES);
 
-  if (keyLength > 0) 
+  if (keyLength > 0)
   {
     memcpy(state->blocks, key, keyLength);
     state->readblock += 128;
@@ -187,7 +179,7 @@ bool Blake2B_Init(Blake2BState *state, const uint8_t outLength,
   return true;
 }
 
-void Blake2B_Hash(Blake2BState *state, const uint8_t *message, const uint64_t messageLength) 
+void Blake2B_Hash(Blake2BState *state, const uint8_t *message, const uint64_t messageLength)
 {
   uint64_t bytesLeft = messageLength;
   uint64_t bytesRead = 0;
@@ -195,16 +187,16 @@ void Blake2B_Hash(Blake2BState *state, const uint8_t *message, const uint64_t me
   int readBytes = 0;
   readBytes = messageLength > BLAKE2B_CONSTANT_BLOCKBYTES ? BLAKE2B_CONSTANT_BLOCKBYTES : messageLength;
   memcpy(state->blocks, message, readBytes);
-
   bytesRead += readBytes;
   state->readblock += readBytes;
+  
 
-  while (bytesLeft > BLAKE2B_CONSTANT_BLOCKBYTES) 
+  while (bytesLeft > BLAKE2B_CONSTANT_BLOCKBYTES)
   {
     Blake2B_Compress(state, false);
-    readBytes = messageLength > BLAKE2B_CONSTANT_BLOCKBYTES ? BLAKE2B_CONSTANT_BLOCKBYTES : messageLength;
+    readBytes = messageLength - bytesRead > BLAKE2B_CONSTANT_BLOCKBYTES ? BLAKE2B_CONSTANT_BLOCKBYTES : messageLength - bytesRead;
 
-    if (readBytes < BLAKE2B_CONSTANT_BLOCKBYTES) 
+    if (readBytes < BLAKE2B_CONSTANT_BLOCKBYTES)
     {
       memset(state->blocks, 0, BLAKE2B_CONSTANT_BLOCKBYTES);
     }
@@ -217,10 +209,10 @@ void Blake2B_Hash(Blake2BState *state, const uint8_t *message, const uint64_t me
   }
 }
 
-void Blake2B_Finalize(Blake2BState *state, uint8_t *outBuffer, const uint64_t outLength) 
+void Blake2B_Finalize(Blake2BState *state, uint8_t *outBuffer, const uint64_t outLength)
 {
   Blake2B_Compress(state, true);
-  if (Is_Big_Endian()) 
+  if (Is_Big_Endian())
   {
     Flip_Uint64_Bytes(&state->stateVector[0]);
     Flip_Uint64_Bytes(&state->stateVector[1]);
@@ -232,19 +224,15 @@ void Blake2B_Finalize(Blake2BState *state, uint8_t *outBuffer, const uint64_t ou
     Flip_Uint64_Bytes(&state->stateVector[7]);
   }
 
-  uint8_t *reader = (uint8_t *)&state->stateVector;
+  uint8_t *reader = (uint8_t*)&state->stateVector;
 
-  for (int i = 0; i < outLength; i++) 
+  for (int i = 0; i < outLength; i++)
   {
     outBuffer[i] = reader[i];
   }
 }
 
-bool Blake2B(const uint8_t *message, const uint64_t messageLength,
-             const uint8_t *key, const uint32_t keyLength, const uint8_t *salt,
-             const uint32_t saltLength, const uint8_t *personalization,
-             const uint32_t personalizationLength, uint8_t *outbuffer,
-             const uint32_t outLength) 
+bool Blake2B(const uint8_t *message, const uint64_t messageLength, const uint8_t *key, const uint32_t keyLength, const uint8_t *salt, const uint32_t saltLength, const uint8_t *personalization, const uint32_t personalizationLength, uint8_t *outbuffer, const uint32_t outLength)
 {
   if (message == NULL && messageLength > 0) return false;
   if (outbuffer == NULL || outLength == 0) return false;
